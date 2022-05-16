39aca28fbd60ba8f432f3cf7a70893ac25fbe60b
YaroslavKNU2024 <86433715+YaroslavKNU2024@users.noreply.github.com>
2022-05-16 02:15:38 +0200

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,26 @@
+using FinalCinema.Controllers;
+using FinalCinema.Models;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public async void Test1()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<CinemaAPIContext>();
+            optionsBuilder.UseSqlServer("Server=DESKTOP-JNBPJFN; Database=FinalCinemaDB; Trusted_Connection=True; MultipleActiveResultSets=true");
+
+            var controller = new GenresController(new CinemaAPIContext(optionsBuilder.Options));
+
+            // Act
+            var result = await controller.GetGenres();
+
+            // Assert
+            Assert.Contains(result.Value, g => g.GenreName.Equals("Thriller"));
+
+        }
+    }
+}
